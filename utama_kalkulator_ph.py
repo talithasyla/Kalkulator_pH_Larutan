5596b43648361e4189653c4d982f1188449393be
talithasyla <168293614+talithasyla@users.noreply.github.com>
2024-05-05 16:23:00 +0700

    Update utama_kalkulator_ph.py

--- utama_kalkulator_ph.py
+++ utama_kalkulator_ph.py
@@ -1,13 +1,13 @@
+from st_pages import Page, show_pages, add_page_title
+
+# Optional -- adds the title and icon to the current page
+add_page_title()
+
 # Specify what pages should be shown in the sidebar, and what their titles and icons
 # should be
 show_pages(
     [
         Page("streamlit_app.py", "Home", "🏠"),
         Page("other_pages/page2.py", "Page 2", ":books:"),
-        Section("My section", icon="🎈️"),
-        # Pages after a section will be indented
-        Page("Another page", icon="💪"),
-        # Unless you explicitly say in_section=False
-        Page("Not in a section", in_section=False)
     ]
 )
