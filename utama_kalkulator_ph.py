5b1888179f5160f64fd640eba924627dabde1c09
talithasyla <168293614+talithasyla@users.noreply.github.com>
2024-05-05 16:11:22 +0700

    Update utama_kalkulator_ph.py

--- utama_kalkulator_ph.py
+++ utama_kalkulator_ph.py
@@ -3,11 +3,11 @@ import streamlit as st
 st.title("KALKULATOR PERHITUNGAN pH")
 
 st.write("Pilih opsi yang akan dihitung!")
-if st.button("Larutan Asam"):
+if st.button("Menghitung dengan Konsentrasi Larutan Asam"):
     st.switch_page("https://kalkulatorphlarutan-c3ewuknkoryyvwbvxuu4g4.streamlit.app/")
-if st.button("Larutan Basa"):
+if st.button("Menghitung dengan Konsentrasi Larutan Basa"):
     st.switch_page("pages/page_1.py")
-if st.button("Massa dan Volume Larutan Asam"):
+if st.button("Menghitung dengan Massa dan Volume Larutan Asam"):
     st.switch_page("pages/page_2.py")
-if st.button("Massa dan Volume Larutan Basa"):
+if st.button("Menghitung dengan Massa dan Volume Larutan Basa"):
     st.switch_page("pages/page_3.py")
