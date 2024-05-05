f6799878d5d54e9fe8f656a1ec11c0271292a14f
talithasyla <168293614+talithasyla@users.noreply.github.com>
2024-05-05 16:27:43 +0700

    Update utama_kalkulator_ph.py

--- utama_kalkulator_ph.py
+++ utama_kalkulator_ph.py
@@ -1,5 +1,33 @@
 import streamlit as st
-from st_pages import Page, show_pages, add_page_title
+from st_pages import show_pages_from_config, add_page_title
 
-show_pages(
-    [ Page("https://www.google.com/webhp?hl=en&sa=X&ved=0ahUKEwjaqImbkfaFAxV4XGcHHRx1ABAQPAgJ", "Home", "🏠"),])
+# Either this or add_indentation() MUST be called on each page in your
+# app to add indendation in the sidebar
+add_page_title()
+
+show_pages_from_config()
+
+[[pages]]
+path = "streamlit_app.py"
+name = "Home"
+icon = "🏠"
+
+[[pages]]
+path = "other_pages/page2.py"
+name = "Page 2"
+icon = ":books:"
+
+[[pages]]
+name = "My second"
+icon = "🎈️"
+is_section = true
+
+# Pages after an `is_section = true` will be indented
+[[pages]]
+name = "Another page"
+icon = "💪"
+
+# Unless you explicitly say in_section = false`
+[[pages]]
+name = "Not in a section"
+in_section = false
