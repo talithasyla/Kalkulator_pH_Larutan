71a558e8c1f98a9e11a05a7ccbdcacafe1adf759
talithasyla <168293614+talithasyla@users.noreply.github.com>
2024-05-05 16:43:08 +0700

    Update utama_kalkulator_ph.py

--- utama_kalkulator_ph.py
+++ utama_kalkulator_ph.py
@@ -4,13 +4,7 @@ from st_pages import Page, add_page_title, show_pages
 
 "## Declaring the pages in your app:"
 
-show_pages(
-    [
-        Page("utama_kalkulator_ph.py", "Home", "🏠"),
-        # Can use :<icon-name>: or the actual icon
-        Page("phlarutanasam_kalkulator_ph.py", "Menghitung pH Larutan Asam", ":books:"),
-        # The pages appear in the order you pass them
-    ]
-)
+show_pages([    Page("utama_kalkulator_ph.py", "Home", "🏠"),
+                Page("phlarutanasam_kalkulator_ph.py", "Menghitung pH Larutan Asam", ":books:"),])
 
 add_page_title()  # Optional method to add title and icon to current page
