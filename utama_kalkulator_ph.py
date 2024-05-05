1b3038729930b046fa4fc651e54c302886f8e164
talithasyla <168293614+talithasyla@users.noreply.github.com>
2024-05-05 16:26:22 +0700

    Update utama_kalkulator_ph.py

--- utama_kalkulator_ph.py
+++ utama_kalkulator_ph.py
@@ -2,5 +2,4 @@ import streamlit as st
 from st_pages import Page, show_pages, add_page_title
 
 show_pages(
-    [ Page("streamlit_app.py", "Home", "🏠"),
-      Page("other_pages/page2.py", "Page 2", ":books:"),])
+    [ Page("https://www.google.com/webhp?hl=en&sa=X&ved=0ahUKEwjaqImbkfaFAxV4XGcHHRx1ABAQPAgJ", "Home", "🏠"),])
