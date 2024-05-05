4c48b1e8ede3fc507dab09a276e76c0fc026213a
talithasyla <168293614+talithasyla@users.noreply.github.com>
2024-05-05 16:14:57 +0700

    Update phlarutanasam_kalkulator_ph.py

--- phlarutanasam_kalkulator_ph.py
+++ phlarutanasam_kalkulator_ph.py
@@ -1,3 +1,18 @@
 import streamlit as st
+import math
 
-st.write("asam is here")
+st.title('Kalkulator pH Larutan')
+
+konsentrasi = st.number_input('Masukkan Konsentrasi',)
+st.write("Konsentrasi = ", konsentrasi)
+valensi = st.number_input('Masukkan Valensi', 0)
+st.write("a = ", valensi)
+hitung = st.button('Hitung pH')
+
+if hitung:
+    asam = konsentrasi * valensi
+    st.write('[H+] = ', asam)
+    log = math.log10(asam)
+    pH = log * -1
+    st.write('pH = ', pH)
+    st.success(f'pH Asam adalah {pH:.2f}')
