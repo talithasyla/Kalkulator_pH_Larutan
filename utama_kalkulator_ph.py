82fbcc5671a567c47885d0734ea2eb903c98f0ad
talithasyla <168293614+talithasyla@users.noreply.github.com>
2024-05-05 16:54:10 +0700

    Update utama_kalkulator_ph.py

--- utama_kalkulator_ph.py
+++ utama_kalkulator_ph.py
@@ -1,7 +1,5 @@
 import streamlit as st
 st.title("Kalkulator pH Larutan")
 
-from st_pages import show_pages_from_config
-
 if st.pages("Kalkulator pH Larutan Asam"):
   st.show_page("https://kalkulatorphlarutan-c3ewuknkoryyvwbvxuu4g4.streamlit.app/", ":home:")
